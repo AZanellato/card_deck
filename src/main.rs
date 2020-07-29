087e25b8ac9e2b04d02a519b90c8c82108f91b42
André Zanellato <andre.zanellato@gmail.com>
2020-07-29 00:02:57 -0300

    Add Diesel and Postgres.
    Add a docker-compose file to run postgres
    and add all the config to Rocket/Diesel to make that
    possible.

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,21 @@
 
 #[macro_use]
 extern crate rocket;
+#[macro_use]
+extern crate rocket_contrib;
 
-#[get("/")]
+use rocket_contrib::databases::diesel;
 
-fn index() -> &'static str {
-    "Hello, world!"
-}
+#[database("postgres_db")]
+struct LogsDbConn(diesel::PgConnection);
 
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite()
+        .attach(LogsDbConn::fairing())
+        .mount("/", routes![index])
+        .launch();
+}
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
 }
