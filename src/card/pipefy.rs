efaf8215ae6348f1d867abb99a0656f3d7fd5433
André Zanellato <andre.zanellato@gmail.com>
2020-09-20 00:48:01 -0300

    Add anyhow, done -> finished_at to reflect attrs.

--- src/card/pipefy.rs
+++ src/card/pipefy.rs
@@ -1,23 +1,53 @@
+use anyhow::Result;
+use chrono::NaiveDateTime;
+// use serde::de::{self, Deserializer};
+use serde_json::Value;
+use std::collections::HashMap;
+use std::error::Error;
+use std::fmt::{self, Formatter};
+
+#[derive(Debug, Clone)]
+struct Unauthorized;
+impl Unauthorized {
+    fn new() -> Unauthorized {
+        Unauthorized {}
+    }
+}
+
+impl Error for Unauthorized {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        None
+    }
+}
+
+impl fmt::Display for Unauthorized {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        write!(f, "Unauthorized access")
+    }
+}
+
 #[derive(Debug, Serialize, Deserialize)]
 pub struct PipefyCard {
     pub title: String,
-    pub done: NaiveDateTime,
     pub created_at: NaiveDateTime,
     pub updated_at: NaiveDateTime,
+    pub finished_at: Option<NaiveDateTime>,
     pub phases_history: Vec<PhaseHistory>,
 }
 
+#[derive(Debug, Serialize, Deserialize)]
 pub struct PhaseHistory {
     firstTimeIn: NaiveDateTime,
     phase: Phase,
 }
 
+#[derive(Debug, Serialize, Deserialize)]
 pub struct Phase {
     name: String,
     id: usize,
 }
 
-pub fn card_query_and_print(api_key: &str, card_id: i32) -> Result<(), Box<Error>> {
+pub fn by_id(api_key: &str, card_id: i32) -> Result<PipefyCard> {
     let mut query: HashMap<&str, String> = HashMap::new();
     let format_card_query_string = format!(
         "query {{
@@ -25,7 +55,7 @@ pub fn card_query_and_print(api_key: &str, card_id: i32) -> Result<(), Box<Error
             title
             createdAt
             finished_at
-            phases_history {
+            phases_history {{
                 firstTimeIn
                 phase {{
                     name
@@ -43,14 +73,14 @@ pub fn card_query_and_print(api_key: &str, card_id: i32) -> Result<(), Box<Error
     match card {
         Ok(card) => {
             dbg!(&card);
-            Ok(())
+            Ok(card)
         }
-        _ => Err(Box::new(Unauthorized::new())),
+        _ => Err(anyhow::Error::new(Unauthorized::new())),
     }
 }
 
 fn perform_query(api_key: &str, query: HashMap<&str, String>) -> Result<String, reqwest::Error> {
-    let client = reqwest::Client::new();
+    let client = reqwest::blocking::Client::new();
     let mut res = client
         .post("https://app.pipefy.com/queries")
         .json(&query)
